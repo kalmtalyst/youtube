0669a4e34012bbebb89cab19a29fbb937fe5925a
kalmtalyst <kalmtalyst@gmail.com>
2022-02-25 12:38:14 +0900

    add video details and style

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./video.css";
 
 function Video(props) {
   return (
-    <div>
-      <li>{props.videoItem.snippet.channelTitle}</li>
-    </div>
+    <li className="videoContainer">
+      <div className="video">
+        <img
+          className="thumImg"
+          src={props.videoItem.snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className="titleData">
+          <p className="title">{props.videoItem.snippet.title}</p>
+          <p className="channelTitle">{props.videoItem.snippet.channelTitle}</p>
+        </div>
+      </div>
+    </li>
   );
 }
 
